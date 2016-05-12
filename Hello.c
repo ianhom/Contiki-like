19fbef61b9560c6e33ac8197c02abafe61984041
Ian Hom <ianhom@163.com>
2016-05-12 17:24:14 +0800

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -12,15 +12,63 @@
 #include "contiki.h"
 #include <stdio.h> 
 
-PROCESS(hello_world_process, "Hello world process");  
-AUTOSTART_PROCESSES(&hello_world_process);
+#ifdef ORIGINAL_CODE
 
-PROCESS_THREAD(hello_world_process, ev, data)
+PROCESS(hello_world_process, "Hello world process");    /* Define a process      */
+AUTOSTART_PROCESSES(&hello_world_process);              /* Make it auto starting */
+
+PROCESS_THREAD(hello_world_process, ev, data)           /* Procss body           */
 {
 
-  PROCESS_BEGIN();
+  PROCESS_BEGIN();                                      /* The start of process  */
 
-  printf("Hello World!\n");
+  printf("Hello World!\n");                             /* Just do it......later */
 
-  PROCESS_END();
+  PROCESS_END();                                        /* The end of process    */
 }
+
+#else
+/********************************************************************************/
+/* Let's translate: */
+/* PROCESS(hello_world_process, "Hello world process"); */
+static char process_thread_hello_world_process(struct pt *process_pt, process_event_t ev, process_data_t data);
+struct process hello_world_process = 
+{
+    NULL,
+    "Hello world process",
+    process_thread_hello_world_process,
+    {0},
+    0,
+    0
+};
+
+/* AUTOSTART_PROCESSES(&hello_world_process); */
+struct process * const autostart_processes[] = 
+{
+    &hello_world_process 
+};
+
+/* PROCESS_THREAD(hello_world_process, ev, data) */
+static char process_thread_hello_world_process(struct pt *process_pt, process_event_t ev, process_data_t data)
+{
+    /* PROCESS_BEGIN(); */
+    {
+        char PT_YIELD_FLAG = 1;
+        if (PT_YIELD_FLAG)
+        {
+            ;
+        }
+        switch (process_pt->lc)
+        {
+            case 0:
+                ;
+                printf("Hello World!\n");
+            
+    /* PROCESS_BEGIN(); */
+        }
+        PT_YIELD_FLAG  = 0;
+        process_pt->lc = 0;
+        return PT_ENDED;
+    };
+}
+#endif
